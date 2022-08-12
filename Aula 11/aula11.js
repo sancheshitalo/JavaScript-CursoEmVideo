8844fb7fa10498b6c24d8451273f0a614ad895b2
Hitalo Sanches <sancheshitalo@gmail.com>
2022-08-12 17:22:08 -0300

    Condições em JS pt1
    Estudando condições em JavaScript pt1.

---
+++ Aula 11/aula11.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel} KM/h.`)
+//Condição simples abaixo.
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`) 
+}
+
+console.log('Dirija sempre usando cinto de segurança.')

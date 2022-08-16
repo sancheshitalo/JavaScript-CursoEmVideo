62070db4504b2b659c46aefa9c58987774d2f3be
Hitalo Sanches <sancheshitalo@gmail.com>
2022-08-16 18:01:09 -0300

    Repetições em JS
    Inicio em repetições em JS.
    Alterações em arquivos.

---
+++ Aula14/ambiente.js
@@ -0,0 +1,17 @@
+console.log('Irá começar...')
+for (var c=1; c<=6; c++) {
+    console.log(c)
+}
+console.log('Finalizado com sucesso.')
+
+
+
+
+
+/*
+var c = 1 
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
